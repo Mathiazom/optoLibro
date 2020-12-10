ee39eb7345ad01f82de116080142df9048c54ec1
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2020-12-10 22:59:57 +0100

    ✍ Renamed main flask file

--- opto.py
+++ opto.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user, login_user, logout_user
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from book_handler import fetch_books
-from models import db, login, UserModel, BookModel
+from models import db, login, User, List, Book
 from views import AdminModelView
 
 app = Flask(__name__)
@@ -16,8 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 admin = Admin(app, name='optolibro',template_mode='bootstrap3')
-admin.add_view(AdminModelView(UserModel, db.session))
-admin.add_view(AdminModelView(BookModel, db.session))
+admin.add_view(AdminModelView(User, db.session))
+admin.add_view(AdminModelView(List, db.session))
+admin.add_view(AdminModelView(Book, db.session))
 
 login.init_app(app)
 login.login_view = 'login'
@@ -28,9 +29,15 @@ def create_table():
     db.create_all()
 
 @app.route("/")
+def lists():
+    lists = List.query.order_by(List.id.desc()).all()
+    return render_template("lists.html", lists=lists)
+
+@app.route("/list")
 def list():
-    books = BookModel.query.order_by(BookModel.ordinal.asc()).all()
-    return render_template("list.html", books=books)
+    list_id = request.args.get("id")
+    list = List.query.filter_by(id=list_id).first()
+    return render_template("list.html", list=list)
 
 @app.route("/fetch")
 def fetch():
@@ -40,9 +47,11 @@ def fetch():
 
     books = fetch_books()
 
+    list = List.query.first()
+
     for i,b in enumerate(books):
 
-        existing_book = BookModel.query.filter_by(name=b[0]).first()
+        existing_book = Book.query.filter_by(name=b[0]).first()
 
         if(existing_book):
             existing_book.ordinal = i+1
@@ -51,10 +60,11 @@ def fetch():
             existing_book.img_url = b[3]
             existing_book.description = b[4][:500]
         else:
-            book = BookModel(ordinal=i+1,name=b[0],author=b[1],url=b[2],img_url=b[3],description=b[4][:500])
-            db.session.add(book)
+            book = Book(ordinal=i+1,name=b[0],author=b[1],url=b[2],img_url=b[3],description=b[4][:500])
+            list.books.append(book)
 
-        db.session.commit()
+    db.session.add(list)
+    db.session.commit()
 
     return redirect("/")
 
@@ -64,7 +74,7 @@ def fulfill():
     if request.method == 'POST':
         book_id = request.args.get("book_id")
         reset = request.args.get("reset")
-        book = BookModel.query.filter_by(id = book_id).first()
+        book = Book.query.filter_by(id = book_id).first()
         book.fulfilled = not reset
         db.session.commit()
     return redirect("/")
@@ -75,7 +85,7 @@ def login():
         return redirect("/")
     if request.method == 'POST':
         username = request.form['username']
-        user = UserModel.query.filter_by(username = username).first()
+        user = User.query.filter_by(username = username).first()
         if user is not None and user.check_password(request.form['password']):
             login_user(user)
             return redirect('/')
@@ -92,13 +102,13 @@ def register():
         password = request.form['password'].strip()
         confirm_password = request.form['confirm_password'].strip()
 
-        if UserModel.query.filter_by(username=username).first():
+        if User.query.filter_by(username=username).first():
             return ('Brukernavnet er allerede i bruk')
 
         if password != confirm_password:
             return ('Passordene stemmer ikke overensen')
 
-        user = UserModel(username=username)
+        user = User(username=username)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
